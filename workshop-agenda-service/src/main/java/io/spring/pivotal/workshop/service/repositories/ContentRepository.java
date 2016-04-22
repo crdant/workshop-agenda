b37a36f0a6dcee1f37f4ba68d733dd79ccf7152e
Chris Decelles <cdecelles@pivotal.io>
2016-04-22 10:00:20 -0400

    remove

--- workshop-agenda-service/src/main/java/io/spring/pivotal/workshop/service/repositories/ContentRepository.java
+++
@@ -1,8 +0,0 @@
-package io.spring.pivotal.workshop.service.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import io.spring.pivotal.workshop.service.domain.Content;
-
-public interface ContentRepository extends CrudRepository<Content, String> {
-}
